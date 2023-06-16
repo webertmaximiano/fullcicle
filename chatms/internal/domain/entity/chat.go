2b5af5e98edff8ee72973ca2bd978d45a6625960
Webert Maximiano <webertcoach@gmail.com>
2023-06-16 19:32:49 -0300

    first commit

---
+++ chatms/internal/domain/entity/chat.go
@@ -0,0 +1,37 @@
+package entity
+
+import "errors"
+
+type ChatConfig struct {
+	Model            *Model
+	Temperature      float32 // pegar detalhes no site da OPenAI
+	TopP             float32
+	N                int
+	Stop             []string
+	MaxTokens        int
+	PresencePenalty  float32
+	FrequencyPenalty float32
+}
+
+type Chat struct {
+	ID                   string
+	UserID               string
+	InitialSystemMessage *Message
+	Messages             []*Message
+	ErasedMessages       []*Message
+	Status               string
+	TokenUsage           int
+	Config               *ChatConfig
+}
+
+func (c *Chat) AddMessage(m *Message) error {
+	if c.Status == "ended" {
+		return errors.New("chat is ended. no more messages allowed")
+	}
+	for {
+		if c.Config.Model.GetMaxTokens() >= m.GetQtdTokens()+c.TokenUsage {
+			c.Messages = append(c.Messages, m)
+		}
+	}
+
+}
